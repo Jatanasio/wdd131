5f95ef8f3a47437984ccc74538260ee7703e7dbc
Jacob Atanasio <156847870+Jatanasio@users.noreply.github.com>
2025-01-25 17:07:55 -0700

    practice

---
+++ review.js
@@ -0,0 +1,17 @@
+// const newP = document.createElement("p");
+// newP.textContent = "Hello World";
+// document.body.append(newP);
+
+const newSection = document.createElement("section");
+document.body.append(newSection);
+
+const h2 = document.createElement("h2");
+h2.textContent = "DOM Basics";
+document.body.append(h2);
+
+const newP = document.createElement("p");
+newP.textContent = "This was added through JavaScript";
+document.body.append(newP);
+
+
+ 
